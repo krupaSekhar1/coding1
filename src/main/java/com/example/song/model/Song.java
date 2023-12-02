eb19ec35d62ef2a3b7adf5890398e0f69f114b73
krupaSekhar1 <krupasekhar124@gmail.com->
2023-12-02 10:33:50 +0000

    solution

---
+++ src/main/java/com/example/song/model/Song.java
@@ -0,0 +1,59 @@
+package com.example.song.model;
+
+import java.util.*;
+
+public class Song {
+    int songId;
+    String songName;
+    String lyricist;
+    String singer;
+    String musicDirector;
+
+    public Song(int songId, String songName, String lyricist, String singer, String musicDirector) {
+        this.songId = songId;
+        this.songName = songName;
+        this.lyricist = lyricist;
+        this.singer = singer;
+        this.musicDirector = musicDirector;
+    }
+
+    public int getSongId() {
+        return songId;
+    }
+
+    public void setSongId(int songId) {
+        this.songId = songId;
+    }
+
+    public String getSongName() {
+        return songName;
+    }
+
+    public void setSongName(String songName) {
+        this.songName = songName;
+    }
+
+    public String getLyricist() {
+        return lyricist;
+    }
+
+    public void setLyriicst(String songName) {
+        this.lyricist = lyricist;
+    }
+
+    public String getSinger() {
+        return singer;
+    }
+
+    public void setSinger(String singer) {
+        this.singer = singer;
+    }
+
+    public String getMusicDirector() {
+        return musicDirector;
+    }
+
+    public void setMusic(String musicDirector) {
+        this.musicDirector = musicDirector;
+    }
+}
---
+++ src/main/java/com/example/song/service/SongH2Service.java
@@ -0,0 +1,72 @@
+package com.example.song.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import java.util.ArrayList;
+import com.example.song.model.*;
+import com.example.song.repository.SongRepository;
+import java.util.*;
+
+@Service
+
+public class SongH2Service implements SongRepository {
+  @Autowired
+  private JdbcTemplate db;
+
+  @Override
+
+  public void deleteSong(int songId) {
+    db.update("delete from playlist where songId=?", songId);
+  }
+
+  @Override
+
+  public Song updateSong(int songId, Song song) {
+    if (song.getSongName() != null) {
+      db.update("update playlist set songName = ? where songId = ?", song.getSongName(), songId);
+    }
+    if (song.getLyricist() != null) {
+      db.update("update playlist set lyricist = ? where songId = ?", song.getLyricist(), songId);
+    }
+
+    if (song.getSinger() != null) {
+      db.update("update playlist set singer = ? where songId = ?", song.getSinger(), songId);
+    }
+    if (song.getMusicDirector() != null) {
+      db.update("update playlist set musicDirector = ? where songId = ?", song.getMusicDirector(), songId);
+    }
+    return getSongById(songId);
+  }
+
+  @Override
+
+  public Song addSong(Song song) {
+    db.update("insert into playlist (songName,lyricist,singer,musicDirector) values (?,?,?,?)", song.getSongName(),
+        song.getLyricist(), song.getSinger(), song.getMusicDirector());
+    Song song1 = db.queryForObject("select * from playlist where songName=? and lyricist=?", new SongRowMapper(),
+        song.getSongName(), song.getLyricist());
+    return song1;
+  }
+
+  @Override
+
+  public Song getSongById(int songId) {
+    try {
+      Song song = db.queryForObject("select * from playlist where songId=?", new SongRowMapper(), songId);
+      return song;
+    } catch (Exception e) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+  }
+
+  @Override
+
+  public ArrayList<Song> getSongs() {
+    List<Song> songList = db.query("select * from playlist", new SongRowMapper());
+    ArrayList<Song> songs = new ArrayList<>(songList);
+    return songs;
+  }
+}
